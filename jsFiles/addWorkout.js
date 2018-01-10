ed8ba384d4f586e3c5a0978f47ce796548aab3e3
Aaron Peressini <steinaua@flip1.engr.oregonstate.edu>
2018-01-09 16:29:24 -0800

    Added workout tracker application files

---
+++ jsFiles/addWorkout.js
@@ -0,0 +1,358 @@
+
+/*
+Aaron Peressini
+6/11/2017
+CS 290
+ */
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+document.getElementById('tracker').style.border = 'medium solid teal';
+document.getElementById('tracker').style.textAlign = 'center';
+var c = document.getElementById('header').children;
+for(var x = 0; x < c.length; x++)
+{
+	c[x].style.borderBottom = 'thin solid teal';
+    	c[x].style.borderRight = 'thin solid teal';
+}
+
+function bindButtons(){
+        document.getElementById('addWorkout').addEventListener('click', function(event){
+          
+          var req = new XMLHttpRequest();
+          var name = document.getElementById('name').value;
+          if(name == "")
+          {
+              alert("Name is required. Please try again.");
+              event.preventDefault();
+              return;
+          }
+          var reps = document.getElementById('reps').value;
+          var weight = document.getElementById('weight').value;
+          var date = document.getElementById('date').value;
+          if(document.getElementsByName('lbs')[0].checked){
+             var lbs = 1;
+          }
+         
+          else{
+              var lbs = 0;
+          }
+    
+          //req.setRequestHeader('Content-Type', 'text/plain');
+          req.open('GET', "http://flip1.engr.oregonstate.edu:5100/insert?name=" + name + "&reps=" + reps + "&weight=" + weight + "&date=" + date + "&lbs=" + lbs, true);
+          req.addEventListener("load", function(){
+          if(req.status >= 200 && req.status < 400)
+          {
+              var response = JSON.parse(req.responseText);
+
+              var newRow = document.createElement("tr");
+              var newName = document.createElement("td");
+              newName.textContent = response[response.length-1].name;
+              newRow.appendChild(newName);
+              var newReps = document.createElement("td");
+              newReps.textContent = response[response.length-1].reps;
+              newRow.appendChild(newReps);
+              var newWeight = document.createElement("td");
+              newWeight.textContent = response[response.length-1].weight;
+              newRow.appendChild(newWeight);
+              var newDate = document.createElement("td");
+              newDate.textContent = response[response.length-1].date.substring(0,10);
+              newRow.appendChild(newDate);
+              var newLbs = document.createElement("td");
+              if(response[response.length-1].lbs == 1){
+                newLbs.textContent = "LB";
+              }
+              else if(response[response.length-1].lbs == 0){
+                  newLbs.textContent = "KG";
+              }
+              
+              newRow.appendChild(newLbs);
+              
+
+              var customizeForm = document.createElement("form");
+              var newID = document.createElement("input");
+              newID.textContent = response[response.length-1].id;
+              newID.setAttribute("type", "hidden");
+              customizeForm.appendChild(newID);     
+
+              var deleteButton = document.createElement("input");
+              deleteButton.id = 'deleteWorkout';
+              deleteButton.tagName='deleteWorkout';
+              deleteButton.value = 'Delete Workout';
+              deleteButton.className = 'deleteButton';
+              deleteButton.type = 'submit';
+              deleteButton.addEventListener('click', callDeleteWorkout);
+              customizeForm.appendChild(deleteButton);
+
+              var editButton = document.createElement("input");
+              editButton.id = 'editWorkout';
+              editButton.value = 'Edit Workout';
+              editButton.className = 'editButton';
+              editButton.type = 'submit';
+              editButton.addEventListener('click', callEditWorkout);
+              customizeForm.appendChild(editButton);
+
+              newRow.appendChild(customizeForm);
+              newRow.id = response[response.length-1].id;
+              newRow.style.border = 'thin solid teal';
+              document.getElementById("tracker").appendChild(newRow);
+
+          }
+          else
+          {
+             console.log("Error in network request: " + req.statusText);
+          }
+              
+                          
+         });
+          event.preventDefault();  
+          req.send(null); 
+                    
+        });
+        
+
+      
+   /*         document.getElementById('editWorkout').addEventListener('click', function(event){
+            
+                var req = new XMLHttpRequest();
+
+                req.open('GET', "http://flip1.engr.oregonstate.edu:5100/insert?name=" + name + "&reps=" + reps + "&weight=" + weight + "&date=" + date + "&lbs=" + lbs, true);
+                req.addEventListener("load", function(){
+                if(req.status >= 200 && req.status < 400)
+                {
+
+                }
+
+                else
+                {
+                console.log("Error in network request: " + req.statusText);
+                }
+
+               // event.preventDefault();  
+                req.send(null); 
+            });
+
+            event.preventDefault();
+        });*/
+    
+
+    
+}
+
+ //document.getElementById('deleteWorkout').addEventListener('click', function(event){
+
+function callDeleteWorkout(event){
+                if(document.getElementById('editForm'))
+                {
+                    document.getElementById('editForm').remove();
+                }
+                var req = new XMLHttpRequest();
+                console.log(event.target.parentNode.textContent);
+                var id = event.target.parentNode.firstElementChild.textContent;
+                req.open('GET', "http://flip1.engr.oregonstate.edu:5100/delete?id=" + id, true);
+                req.addEventListener("load", function(){
+                if(req.status >= 200 && req.status < 400)
+                {
+                    console.log(req.responseText);
+                    document.getElementById(id).remove();
+                }
+
+                else
+                {
+                    console.log("Error in network request: " + req.statusText);
+                }
+                
+                            
+            });
+
+            event.preventDefault();  
+            req.send(id); 
+}
+
+
+function callEditWorkout(event){
+
+        var ebuttons = document.getElementsByClassName('editButton');
+        for(var x = 0; x < ebuttons.length; x++)
+        {
+            ebuttons[x].disabled = true;
+        }
+        ebuttons = document.getElementsByClassName('deleteButton');
+        for(var x = 0; x < ebuttons.length; x++)
+        {
+            ebuttons[x].disabled = true;
+        }
+        
+
+        console.log(event.target.parentNode.textContent);
+        var id = event.target.parentNode.firstElementChild.textContent;
+
+        var curRow = event.target.parentNode.parentNode;
+        var curElem = curRow.firstElementChild;
+
+        var editForm = document.createElement("form");
+        editForm.id = 'editForm';
+        editForm.textContent = 'Edit workout ';
+        var newID = document.createElement("input");
+        newID.textContent = id;
+        newID.setAttribute("type", "hidden");
+        editForm.appendChild(newID); 
+        var name = document.createElement("input");
+        name.type = 'text';
+        name.value = curElem.textContent;      
+        name.textContent = 'Workout Name';  
+        curElem = curElem.nextElementSibling;
+        editForm.appendChild(name);
+        var reps = document.createElement("input");
+        reps.type = 'number';
+        reps.value = curElem.textContent;
+        reps.textContent = 'Reps';
+        curElem = curElem.nextElementSibling;
+        editForm.appendChild(reps);
+        var weight = document.createElement("input");
+        weight.type = 'number';
+        weight.value = curElem.textContent;
+        weight.textContent = 'Weight';
+        curElem = curElem.nextElementSibling;
+        editForm.appendChild(weight);
+        var date = document.createElement("input");
+        date.type = 'text';
+        date.value = curElem.textContent;
+        date.textContent = 'Date Performed';
+        curElem = curElem.nextElementSibling;
+        editForm.appendChild(date);
+        var lb = document.createElement("input");
+        lb.type = 'radio';
+        lb.name = 'lbs';
+        lb.value = 1;
+        lb.textContent = 'LB';
+        var kg = document.createElement("input");
+        kg.type = 'radio';
+        kg.name = 'lbs';
+        kg.value = 0;
+        kg.textContent = 'KG';
+        var units = document.createElement('input');
+        units.type = 'hidden'
+        units.value = curElem.textContent;
+      
+        editForm.appendChild(lb);
+        editForm.appendChild(kg);
+        editForm.appendChild(units);
+
+        var updateButton = document.createElement('input');
+        updateButton.type = 'submit';
+        updateButton.value= 'Update Workout';
+        updateButton.id = 'updateWorkout';
+        updateButton.addEventListener('click', function(e){
+
+
+
+                var req = new XMLHttpRequest();
+                var id = e.target.parentNode.firstElementChild.textContent;
+                
+
+                var par = e.target.parentNode;
+                var curElem = par.firstElementChild;
+                curElem = curElem.nextElementSibling;
+
+                var name = curElem.value;
+                curElem = curElem.nextElementSibling;
+                var reps = curElem.value;
+                curElem = curElem.nextElementSibling;
+                var weight = curElem.value;
+                curElem = curElem.nextElementSibling;
+                var date = curElem.value;
+                curElem = curElem.nextElementSibling;
+
+                var lbs;
+                if(curElem.nextElementSibling.nextElementSibling.value == 'LB')
+                {
+                    lbs = 1;
+                }
+                if(curElem.nextElementSibling.nextElementSibling.value == 'KG')
+                {
+                    lbs = 0;
+                }
+                if(curElem.checked)
+                {
+                    lbs = 1;
+                }
+
+                if(curElem.nextElementSibling.checked)
+                {
+                    lbs = 0;
+                }
+                req.open('GET', "http://flip1.engr.oregonstate.edu:5100/safe-update?id=" + id + "&name=" + name + "&reps=" + reps + "&weight=" + weight + "&date=" + date + "&lbs=" + lbs, true);
+                req.addEventListener("load", function(){
+                if(req.status >= 200 && req.status < 400)
+                {
+                   
+                    var response = JSON.parse(req.responseText);
+                    console.log(response);//req.responseText);
+                    document.getElementById('editForm').remove();
+                    document.getElementById(id).style.backgroundColor = 'transparent';
+
+                    for(var x = 0; x < response.length; x++)
+                    {
+                        if(response[x].id == id)
+                        {
+                            var curRow = document.getElementById(id);
+                            var curElem = curRow.firstChild
+
+                            curElem.textContent = response[x].name;
+                            curElem = curElem.nextElementSibling;
+                            curElem.textContent = response[x].reps;
+                            curElem = curElem.nextElementSibling;
+                            curElem.textContent = response[x].weight;
+                            curElem = curElem.nextElementSibling;
+                            curElem.textContent = response[x].date.substring(0,10);
+                            curElem = curElem.nextElementSibling;
+                            if(response[x].lbs == 1){
+                                curElem.textContent = "LB";
+                            }
+                            else if(response[x].lbs == 0){
+                                curElem.textContent = "KG";
+                            }
+                        }
+                    }
+
+                    var ebuttons = document.getElementsByClassName('editButton');
+                    for(var x = 0; x < ebuttons.length; x++)
+                    {
+                        ebuttons[x].disabled = false;
+                    }
+                    ebuttons = document.getElementsByClassName('deleteButton');
+                    for(var x = 0; x < ebuttons.length; x++)
+                    {
+                        ebuttons[x].disabled = false;
+                    }
+                                     
+                }
+
+                else
+                {
+                    console.log("Error in network request: " + req.statusText);
+                }
+                
+                            
+            });
+
+            e.preventDefault();  
+            req.send(id); 
+
+
+
+        });
+
+        editForm.appendChild(updateButton);
+
+        curRow.style.backgroundColor = 'yellow';
+
+        document.body.appendChild(editForm);
+
+        event.preventDefault();
+        //curRow.insertAdjacentElement('afterend', editForm);
+
+}
+        
+ 
---
+++ workoutTracker.js
@@ -0,0 +1,151 @@
+
+/*
+Aaron Peressini
+6/11/2017
+CS 290
+ */
+
+var express = require('express');
+var mysql = require('./dbcon.js');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5100);
+app.use(express.static('assets'));
+
+app.get('/',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+    var createString = "CREATE TABLE todo(" +
+    "id INT PRIMARY KEY AUTO_INCREMENT," +
+    "name VARCHAR(255) NOT NULL," +
+    "reps INT," +
+    "weight INT," +
+    "date DATE," +
+    "lbs BOOLEAN)";
+    mysql.pool.query(createString, function(err){
+      //context.results = "Table reset";
+      res.render('home',context);
+    });
+  });
+ 
+ /* var context = {};
+  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    //context.results = rows;
+    res.render('home', context);
+  });  */ 
+});
+
+app.get('/insert',function(req,res,next){
+  var context = {};
+  mysql.pool.query("INSERT INTO todo (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    console.log("Inserted");
+    //context.results = result.insertId;
+    //console.log(context.results); 
+   // res.send(context.results.toString()); 
+   mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+      if(err){
+        next(err);
+        return;
+      }
+      //res.type('text/plain');
+      //context.results = rows; 
+      res.json(rows);
+   });
+
+  });
+
+ 
+});
+
+app.get('/delete',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DELETE FROM todo WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Deleted " + result.changedRows + " rows.";
+    //console.log("hello");
+    res.json(context.results);
+  });
+});
+
+
+///safe-update?id=1&name=The+Task&done=false
+app.get('/safe-update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("SELECT * FROM todo WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      var curVals = result[0];
+      mysql.pool.query("UPDATE todo SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+        [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, 
+        req.query.lbs || curVals.lbs, req.query.id],
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+        //context.results = "Updated " + result.changedRows + " rows.";
+       // res.render('home',context);
+        mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+            if(err){
+              next(err);
+              return;
+            }
+            //res.type('text/plain');
+            context.results = rows;
+            //console.log(context.results);    
+            res.json(rows);
+        });
+      });
+    }
+  });
+});
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+    var createString = "CREATE TABLE todo(" +
+    "id INT PRIMARY KEY AUTO_INCREMENT," +
+    "name VARCHAR(255) NOT NULL," +
+    "reps INT," +
+    "weight INT," +
+    "date DATE," +
+    "lbs BOOLEAN)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
