8238ac08ac59dd0035bc613d00b50dbbcf09cb8d
Aaron Peressini <steinaua@oregonstate.edu>
2018-01-11 11:53:53 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_steinaua',
-  password        : '3172',
-  database        : 'cs340_steinaua'
+  host            : '********',
+  user            : '********',
+  password        : '********',
+  database        : '********'
 });
 
 module.exports.pool = pool;
