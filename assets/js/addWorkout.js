00bf7aeb047443d8e2c8efb267cc2f1527c4e693
Aaron Peressini <steinaua@oregonstate.edu>
2018-01-19 12:49:36 -0800

    Updated hostname

--- assets/js/addWorkout.js
+++ assets/js/addWorkout.js
@@ -17,8 +17,8 @@ for(var x = 0; x < c.length; x++)
 }
 
 var count = 1;
-var hostname = "https://whispering-ocean-26663.herokuapp.com:";
-var port = "5100";
+var hostname = "https://whispering-ocean-26663.herokuapp.com";
+var port = "";
 
 function bindButtons(){
         document.getElementById('addWorkout').addEventListener('click', function(event){
