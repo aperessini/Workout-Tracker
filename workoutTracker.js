7f3e5faf15c5fc22fadb50690412e13b29e9942d
Aaron Peressini <steinaua@oregonstate.edu>
2018-01-19 13:12:26 -0800

    Addess CORS credentials

--- workoutTracker.js
+++ workoutTracker.js
@@ -16,6 +16,13 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 app.set('port', 5100);
 app.use(express.static('assets'));
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.get('/',function(req,res,next){
   var context = {};
