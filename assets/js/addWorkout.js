2f3cda09c6cb92d9d74f99ee4456f8a0b8be0a3a
Aaron Peressini <steinaua@oregonstate.edu>
2018-01-19 18:48:51 -0800

    Removed port from get request

--- assets/js/addWorkout.js
+++ assets/js/addWorkout.js
@@ -18,7 +18,7 @@ for(var x = 0; x < c.length; x++)
 
 var count = 1;
 var hostname = "https://whispering-ocean-26663.herokuapp.com:";
-var port = document.getElementById('port').value;
+var port = ""; //document.getElementById('port').value;
 
 function bindButtons(){
         document.getElementById('addWorkout').addEventListener('click', function(event){
